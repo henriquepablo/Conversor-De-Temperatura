0910bbbd7b82998cdc82ebd3a29be943f7cd63ee
henriquepablo <pablo269704@gmail.com>
2022-03-25 08:55:42 -0300

    Criando a caixa de texto

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Program {
 
@@ -9,13 +10,16 @@ public class Program {
 		
 		JFrame jf = new JFrame("Conversor");
 		
-		JLabel label = new JLabel("Temperaura em Fahrenheit:");
+		JLabel label = new JLabel("Temperaura em Fahrenheit");
 
 		label.setBounds(50, 10, 170, 20);
 		
+		JTextField tf = new JTextField();
+		
+		tf.setBounds(80, 50, 90, 20);
 		
 		
-		jf.add(label);
+		jf.add(label); jf.add(tf);
 		
 		jf.setSize(275, 275);
 		
