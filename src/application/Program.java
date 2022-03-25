3ee5735364718173b967ba1b00dc9ea186280fc3
henriquepablo <pablo269704@gmail.com>
2022-03-25 08:42:23 -0300

    Criando a label e posicionando na janela

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Program {
 
@@ -8,10 +9,23 @@ public class Program {
 		
 		JFrame jf = new JFrame("Conversor");
 		
+		JLabel label = new JLabel("Temperaura em Fahrenheit:");
+
+		label.setBounds(50, 10, 170, 20);
 		
-		jf.setVisible(true);
+		
+		
+		jf.add(label);
+		
+		jf.setSize(275, 275);
+		
+		jf.setLayout(null);
+		
+		jf.setLocationRelativeTo(null); // centralizar a janela na tela.
+		
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		jf.setVisible(true);
 	}
 
 }
