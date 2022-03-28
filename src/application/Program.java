307257528bf7f2ca3c6260969dd055a1a5ed563a
henriquepablo <pablo269704@gmail.com>
2022-03-28 13:14:51 -0300

    Tratamento de erro e exibindo mensagem

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,13 @@ package application;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.InputMismatchException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class Program {
 
@@ -33,23 +35,28 @@ public class Program {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
-				if (tf.getText().isEmpty() == false) {
-					double fahrenheit = Double.parseDouble(tf.getText());
-					double celsius = (fahrenheit - 32) * 5 / 9;
+				try {
+					if (tf.getText().isEmpty() == false) {
+						double fahrenheit = Double.parseDouble(tf.getText());
+						double celsius = (fahrenheit - 32) * 5 / 9;
 					
-					mensagem.setText(fahrenheit + "°F para celsius:  " + String.format("%.0f", celsius) + " °C");	
-				}
-				else {
-					mensagem.setText("O campo está vazio");
-				}
+						mensagem.setText(fahrenheit + "°F para celsius:  " + String.format("%.0f", celsius) + " °C");	
+					}
 				
+					else {
+						mensagem.setText("O campo está vazio");
+					}
+				}
+				catch (NumberFormatException a) {
+						mensagem.setText("Digite um número");
+				}
 			}
 		});
 		
 		
 		
 		
-		jf.add(label); jf.add(tf); jf.add(button); jf.add(mensagem);
+		jf.add(label); jf.add(tf); jf.add(button); jf.add(mensagem, SwingConstants.CENTER);
 		
 		jf.setSize(275, 275);
 		
