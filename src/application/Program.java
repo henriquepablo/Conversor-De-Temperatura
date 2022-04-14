3667e6760a52ef0a23352725c0d946fc9bad34c7
Pablo Henrique <pablo269704@gmai.com>
2022-04-14 07:04:41 -0300

    fazendo ajustes e modificando o aplicativo

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,8 +5,11 @@ import java.awt.event.ActionListener;
 import java.util.InputMismatchException;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
@@ -16,21 +19,54 @@ public class Program {
 		
 		JFrame jf = new JFrame("Conversor");
 		
-		JLabel label = new JLabel("Temperaura em Fahrenheit");
+		String[] opcoes = new String[]{"Â°C", "Â°F"};
+		
+		JComboBox<String> op = new JComboBox<>(opcoes);
+		
+		op.setBounds(10, 40, 40, 20);
+		
+		
+		JButton button1 = new JButton("1");
+		JButton button2 = new JButton("2");
+		JButton button3 = new JButton("3");
+		JButton button4 = new JButton("4");
+		JButton button5 = new JButton("5");
+		JButton button6 = new JButton("6");
+		JButton button7 = new JButton("7");
+		JButton button8 = new JButton("8");
+		JButton button9 = new JButton("9"); 
+		JButton button = new JButton("0");
+		JButton virgula = new JButton(".");
+		JButton converter = new JButton("cli");
+		
+		button1.setBounds(10, 270 , 50, 50);
+		button2.setBounds(70, 270, 50, 50);
+		button3.setBounds(130, 270, 50, 50);
+		button4.setBounds(10, 210, 50, 50);
+		button5.setBounds(70, 210, 50, 50);
+		button6.setBounds(130, 210, 50, 50);
+		button7.setBounds(10, 150, 50, 50);
+		button8.setBounds(70, 150, 50, 50);
+		button9.setBounds(130, 150, 50, 50);
+		button.setBounds(70, 330, 50, 50);
+		virgula.setBounds(10, 330, 50, 50);
+		converter.setBounds(130, 330, 50, 50);
 
-		label.setBounds(50, 10, 170, 20);
+		jf.add(button);
+		jf.add(button1);
+		jf.add(button2);
+		jf.add(button3);
+		jf.add(button4);
+		jf.add(button5);
+		jf.add(button6);
+		jf.add(button7);
+		jf.add(button8);
+		jf.add(button9);
+		jf.add(virgula);
+		jf.add(converter);
+		jf.add(op);
 		
-		JTextField tf = new JTextField();
-		
-		tf.setBounds(80, 50, 90, 20);
-		
-		JButton button = new JButton("Clique");
-		button.setBounds(90, 180, 70, 20);
-		
-		JLabel mensagem = new JLabel();
-		mensagem.setBounds(70, 112, 170, 20);
-		
-		button.addActionListener(new ActionListener() {
+		/*button.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -40,25 +76,26 @@ public class Program {
 						double fahrenheit = Double.parseDouble(tf.getText());
 						double celsius = (fahrenheit - 32) * 5 / 9;
 					
-						mensagem.setText(fahrenheit + "°F para celsius:  " + String.format("%.0f", celsius) + " °C");	
+						mensagem.setText(fahrenheit + "Â°F para celsius:  " + String.format("%.0f", celsius) + " Â°C");	
 					}
 				
 					else {
-						mensagem.setText("O campo está vazio");
+						mensagem.setText("O campo estÃ¡ vazio");
 					}
 				}
 				catch (NumberFormatException a) {
-						mensagem.setText("Digite um número");
+						mensagem.setText("Digite um nÃºmero");
 				}
 			}
-		});
+		});*/
 		
 		
 		
 		
-		jf.add(label); jf.add(tf); jf.add(button); jf.add(mensagem, SwingConstants.CENTER);
 		
-		jf.setSize(275, 275);
+		//jf.add(label); jf.add(tf); jf.add(button); jf.add(mensagem, SwingConstants.CENTER);
+		
+		jf.setSize(265, 420);
 		
 		jf.setResizable(false);
 		
